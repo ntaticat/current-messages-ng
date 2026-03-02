debe9f22a0bfcedd75a0143dbca41c6d8d42cfac
Rafael Estrada <ntaticat@gmail.com>
2026-03-02 01:08:29 -0600

    pwa

--- src/main.ts
+++ src/main.ts
@@ -5,6 +5,8 @@ import { AppComponent } from './app/app.component';
 import { routes } from './app/app.routes';
 import { provideHttpClient, withInterceptors } from '@angular/common/http';
 import { authInterceptor } from './app/core/interceptors/auth.interceptor';
+import { isDevMode } from '@angular/core';
+import { provideServiceWorker } from '@angular/service-worker';
 
 bootstrapApplication(AppComponent, {
   providers: [
@@ -12,7 +14,10 @@ bootstrapApplication(AppComponent, {
     provideRouter(routes),
 
     // 2. HTTP con interceptores funcionales (más rápido que DI)
-    provideHttpClient(withInterceptors([authInterceptor])),
+    provideHttpClient(withInterceptors([authInterceptor])), provideServiceWorker('ngsw-worker.js', {
+            enabled: !isDevMode(),
+            registrationStrategy: 'registerWhenStable:30000'
+          }),
 
     // 3. Animaciones sin necesidad de cargar todo el módulo
     // provideAnimations(),
